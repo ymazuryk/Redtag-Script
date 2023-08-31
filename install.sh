9c3b775347b11e7f93cae4435c1245582372f555
Yaroslav Mazuryk <yaroslav.mazuryk@redtag.pro>
2023-08-31 15:46:58 +0300

    Fixed NPSP Script with latest version

--- install.sh
+++ install.sh
@@ -15,15 +15,27 @@ echo ""
 # Usage:    getLatestVersion repo
 # Example:  getLatestVersion SalesforceFoundation/Cumulus
 getLatestVersion() {
-  VERSION="$(curl -s "https://api.github.com/repos/$1/releases/latest" | jq -r .name)"
+  VERSION="$(curl -s "https://api.github.com/repos/SalesforceFoundation/$1/releases/latest" | jq -r .name)"
   echo $VERSION
+}
+# Check and Update release version
+# Make Sure Package Id is uptodate
+getLatestVersionNPSP() {
+  VERSION="$(curl -s "https://api.github.com/repos/SalesforceFoundation/$1/releases/115648469" | jq .body)"
+  re="installPackage\.apexp\?p0=(04t[a-zA-Z0-9]+)[\\\r\\\n|\"]"
+  if [[ $VERSION =~ $re ]]; then
+    echo ${BASH_REMATCH[1]};
+  else
+    echo "Could not extract package Id from repo: $REPO"
+    exit -1;
+  fi
 }
 
 # Get the Salesforce package Id (04t....) from the latest version of a GitHub repository
 # Usage:    getLatestPackageId repo
 # Example:  getLatestPackageId SalesforceFoundation/Cumulus
 getLatestPackageId() {
-  VERSION="$(curl -s "https://api.github.com/repos/$1/releases/latest" | jq .body)"
+  VERSION="$(curl -s "https://api.github.com/repos/SalesforceFoundation/$1/releases/latest" | jq .body)"
   re="installPackage\.apexp\?p0=(04t[a-zA-Z0-9]+)[\\\r\\\n|\"]"
   if [[ $VERSION =~ $re ]]; then
     echo ${BASH_REMATCH[1]};
@@ -34,13 +46,23 @@ getLatestPackageId() {
 }
 
 ### BEGIN DEPENDENCIES
-CONTACTS_AND_ORGANIZATIONS_PACKAGE="$(getLatestPackageId SalesforceFoundation/Contacts_and_Organizations)"
-HOUSEHOLDS_PACKAGE="$(getLatestPackageId SalesforceFoundation/Households)"
-RECURRING_DONATIONS_PACKAGE="$(getLatestPackageId SalesforceFoundation/Recurring_Donations)"
-RELATIONSHIPS_PACKAGE="$(getLatestPackageId SalesforceFoundation/Relationships)"
-AFFILIATIONS_PACKAGE="$(getLatestPackageId SalesforceFoundation/Affiliations)"
-NPSP_CORE_PACKAGE="$(getLatestPackageId SalesforceFoundation/Cumulus)"
-NPSP_CORE_VERSION="$(getLatestVersion SalesforceFoundation/Cumulus)"
+CONTACTS_AND_ORGANIZATIONS_PACKAGE="$(getLatestPackageId Contacts_and_Organizations)"
+CONTACTS_AND_ORGANIZATIONS_PACKAGE_VERSION="$(getLatestVersion Contacts_and_Organizations)"
+
+HOUSEHOLDS_PACKAGE="$(getLatestPackageId Households)"
+HOUSEHOLDS_PACKAGE_VERSION="$(getLatestVersion Households)"
+
+RECURRING_DONATIONS_PACKAGE="$(getLatestPackageId Recurring_Donations)"
+RECURRING_DONATIONS_PACKAGE_VERSION="$(getLatestVersion Recurring_Donations)"
+
+RELATIONSHIPS_PACKAGE="$(getLatestPackageId Relationships)"
+RELATIONSHIPS_PACKAGE_VERSION="$(getLatestVersion Relationships)"
+
+AFFILIATIONS_PACKAGE="$(getLatestPackageId Affiliations)"
+AFFILIATIONS_PACKAGE_VERSION="$(getLatestVersion Affiliations)"
+
+NPSP_CORE_PACKAGE="$(getLatestVersionNPSP NPSP)"
+NPSP_CORE_VERSION="$(getLatestVersion NPSP)"
 ### END DEPENDENCIES
 
 echo "Dependencies (automatically fetched from GitHub):"
@@ -50,12 +72,30 @@ echo "  Households $HOUSEHOLDS_PACKAGE"
 echo "  Recurring Donations $RECURRING_DONATIONS_PACKAGE"
 echo "  Relationships $RELATIONSHIPS_PACKAGE"
 echo "  Affiliations $AFFILIATIONS_PACKAGE"
-echo "  Cumulus $NPSP_CORE_PACKAGE ($NPSP_CORE_VERSION)"
+echo "  NPSP $NPSP_CORE_PACKAGE"
 echo ""
+
+echo "Installing Contacts & Organizations Latest Version 1/6 " $CONTACTS_AND_ORGANIZATIONS_PACKAGE_VERSION
+sfdx package install -p $CONTACTS_AND_ORGANIZATIONS_PACKAGE -w 5 -u $ORG_ALIAS --noprompt && \
+
+echo "Installing Contacts & Household Latest Version 2/6  " $HOUSEHOLDS_PACKAGE_VERSION
+sfdx package install -p $HOUSEHOLDS_PACKAGE -w 5 -u $ORG_ALIAS --noprompt && \
+
+echo "Installing Contacts & RECURRING_DONATIONS_PACKAGE Latest Version 3/6 " $RECURRING_DONATIONS_PACKAGE_VERSION 
+sfdx package install -p $RECURRING_DONATIONS_PACKAGE -w 5 -u $ORG_ALIAS --noprompt && \
+
+echo "Installing Contacts & RELATIONSHIPS_PACKAGE Latest Version 4/6 " $RELATIONSHIPS_PACKAGE_VERSION
+sfdx package install -p $RELATIONSHIPS_PACKAGE -w 5 -u $ORG_ALIAS --noprompt && \
+
+echo "Installing Contacts & AFFILIATIONS_PACKAGE Latest Version 5/6 " $AFFILIATIONS_PACKAGE_VERSION
+sfdx package install -p $AFFILIATIONS_PACKAGE -w 5 -u $ORG_ALIAS --noprompt && \
+
+echo "Installing Contacts & NPSP_CORE_PACKAGE Latest Version 5/6 " $NPSP_CORE_VERSION
+sfdx package install -p $NPSP_CORE_PACKAGE -w 5 -u $ORG_ALIAS --noprompt && \
 
 # Reset current path to be relative to script file
 SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-echo $SCRIPT_PATH
+cd $SCRIPT_PATH
 
 
 # With Step 8 installing the npsp managed package, this updates the token with the prefix
